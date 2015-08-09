aad93107565292a0b6a98646facbdab0c660634a
Lucas Rumney <lucasrumney94@gmail.com>
2015-08-08 23:35:08 -0400

    made barebones singleton;

---
+++ Unity Sound Manager/Assets/scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager soundManager;
+	
+	void Awake()												//Singleton Awake() method
+	{
+		if(soundManager == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			soundManager = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
